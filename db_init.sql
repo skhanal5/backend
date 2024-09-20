b1f50fe15c19a79f66eb08e4afae5c7422ade69a
Subodh Khanal <skhanal5@terpmail.umd.edu>
2024-09-20 18:52:25 -0400

    Fix sql script

--- db_init.sql
+++ db_init.sql
@@ -1,5 +1,5 @@
-CREATE TABLE IF NOT EXISTS 'users' (
-    id UUID PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS users (
+    id varchar(255) PRIMARY KEY,
     email_address varchar(255),
     first_name varchar(255),
     last_name varchar(255)
