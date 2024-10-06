8cdbf8dd668e5fcde13accae3571bcb15afaf236
Subodh Khanal <skhanal5@terpmail.umd.edu>
2024-10-05 20:29:17 -0400

    WIP test for auth controller, register working as intended, login not working

--- src/test/java/com/skhanal5/backend/controller/AuthControllerTest.java
+++ src/test/java/com/skhanal5/backend/controller/AuthControllerTest.java
@@ -1,52 +1,72 @@
 package com.skhanal5.backend.controller;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.skhanal5.backend.ResourceFetcher;
-import com.skhanal5.backend.entity.User;
+import com.skhanal5.backend.configuration.SecurityConfig;
 import com.skhanal5.backend.model.AccountDetails;
 import com.skhanal5.backend.repository.UserRepository;
 import java.util.Optional;
+
+import com.skhanal5.backend.service.UserService;
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+@Import(SecurityConfig.class)
 @WebMvcTest(AuthController.class)
 public class AuthControllerTest {
 
-	@Autowired MockMvc mockMvc;
+	@Autowired
+	private WebApplicationContext context;
 
-	@MockBean UserRepository userRepository;
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	UserService userService;
+
+	@MockBean
+	AuthenticationManager authenticationManager;
 
 	@Test
 	void testRegisterUser() throws Exception {
-		var requestBody = ResourceFetcher.getResourceFileAsString("login_request.json");
-		var mockAccountDetails = mock(AccountDetails.class);
-		var insertedUser = new User(mockAccountDetails);
-		when(userRepository.save(any())).thenReturn(insertedUser);
-
+		var requestBody = ResourceFetcher.getResourceFileAsString("registration_request.json");
 		mockMvc
 				.perform(
 						post("/api/v1/register").contentType(MediaType.APPLICATION_JSON).content(requestBody))
 				.andExpect(status().isOk()) // Update this to created
-				.andExpect(content().string("Added new user"));
+				.andExpect(content().string("Created user"));
 	}
 
 	@Test
 	void testLoginUser() throws Exception {
-		var requestBody = ResourceFetcher.getResourceFileAsString("registration_request.json");
-		var mockAccountDetails = mock(AccountDetails.class);
-		var foundUser = new User(mockAccountDetails);
-		when(userRepository.findByEmailAddressAndPassword(any(), any()))
-				.thenReturn(Optional.of(foundUser));
+		var requestBody = ResourceFetcher.getResourceFileAsString("login_request.json");
+		var user = mock(UserDetails.class);
+		when(userService.loadUserByUsername("foo@bar.com"))
+				.thenReturn(user);
 
 		mockMvc
 				.perform(post("/api/v1/login").contentType(MediaType.APPLICATION_JSON).content(requestBody))
