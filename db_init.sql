5c21f41af5c3e20bb6a01720cad93279a741815e
Subodh Khanal <skhanal5@terpmail.umd.edu>
2024-10-05 20:00:40 -0400

    Add password field

--- db_init.sql
+++ db_init.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS users (
     id varchar(255) PRIMARY KEY,
     email_address varchar(255),
+    password varchar(255),
     first_name varchar(255),
     last_name varchar(255)
 );
--- src/main/java/com/skhanal5/backend/configuration/SecurityConfig.java
+++ src/main/java/com/skhanal5/backend/configuration/SecurityConfig.java
@@ -1,11 +1,14 @@
 package com.skhanal5.backend.configuration;
 
+import com.skhanal5.backend.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,6 +17,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+@EnableWebSecurity
 @Configuration
 public class SecurityConfiguration {
 
@@ -29,5 +33,20 @@ public class SecurityConfiguration {
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder(16);
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
+		//make a provider
+		DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+		authenticationProvider.setPasswordEncoder(passwordEncoder);
+		authenticationProvider.setUserDetailsService(userDetailsService);
+
+		return new ProviderManager(authenticationProvider);
+	}
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+		return new UserService();
 	}
 }
