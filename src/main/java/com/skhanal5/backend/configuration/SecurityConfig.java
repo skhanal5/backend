d66fb18ba4e4bc219d8352aae918d9755a6aaf31
Subodh Khanal <skhanal5@terpmail.umd.edu>
2024-10-05 20:01:28 -0400

    Add security configuration

--- src/main/java/com/skhanal5/backend/configuration/SecurityConfig.java
+++ src/main/java/com/skhanal5/backend/configuration/SecurityConfig.java
@@ -1,32 +1,33 @@
 package com.skhanal5.backend.configuration;
 
-import com.skhanal5.backend.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-@EnableWebSecurity
 @Configuration
-public class SecurityConfiguration {
+// @EnableWebSecurity
+public class SecurityConfig {
 
 	@Bean
 	public SecurityFilterChain web(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests((authorize) -> authorize
-                .requestMatchers("/register").permitAll()
-                .requestMatchers("/login").permitAll()
-                .anyRequest().authenticated());
+		http.authorizeHttpRequests(
+						(authorize) ->
+								authorize
+										.requestMatchers("api/v1/register")
+										.permitAll()
+										.requestMatchers("api/v1/login")
+										.permitAll()
+										.anyRequest()
+										.authenticated())
+				.csrf(AbstractHttpConfigurer::disable);
 		return http.build();
 	}
 
@@ -36,17 +37,13 @@ public class SecurityConfiguration {
 	}
 
 	@Bean
-	public AuthenticationManager authenticationManager(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
-		//make a provider
+	public AuthenticationManager authenticationManager(
+			UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
+		// make a provider
 		DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
 		authenticationProvider.setPasswordEncoder(passwordEncoder);
 		authenticationProvider.setUserDetailsService(userDetailsService);
 
 		return new ProviderManager(authenticationProvider);
-	}
-
-	@Bean
-	public UserDetailsService userDetailsService() {
-		return new UserService();
 	}
 }
