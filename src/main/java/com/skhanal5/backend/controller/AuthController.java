fea001bb2d0328198b4f58af800db5754a79916f
Subodh Khanal <skhanal5@terpmail.umd.edu>
2024-10-05 20:01:45 -0400

    Add auth controller

--- src/main/java/com/skhanal5/backend/controller/AuthController.java
+++ src/main/java/com/skhanal5/backend/controller/AuthController.java
@@ -1,40 +1,44 @@
 package com.skhanal5.backend.controller;
 
+import com.skhanal5.backend.model.AccountDetails;
 import com.skhanal5.backend.model.LoginDetails;
-import com.skhanal5.backend.repository.UserRepository;
+import com.skhanal5.backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1")
-public class RegistrationController {
+public class AuthController {
 
-	@Autowired private UserRepository userRepository;
+	@Autowired AuthenticationManager authenticationManager;
+
+	@Autowired UserService service;
 
 	@PostMapping("/register")
-	public String addUser(@RequestBody UserDetails userDetails) {
-
-		return "Added new user";
+	public String addUser(@RequestBody AccountDetails accountDetails) {
+		service.registerNewUser(accountDetails);
+		return "Created user";
 	}
-
-	private String hashPasswords() {}
 
 	@PostMapping("/login")
 	public String loginUser(@RequestBody LoginDetails loginDetails) {
-		var result =
-				userRepository.findByEmailAddressAndPassword(
+		Authentication authRequest =
+				UsernamePasswordAuthenticationToken.unauthenticated(
 						loginDetails.getEmailAddress(), loginDetails.getPassword());
-		if (result.isEmpty()) {
-			throw new IllegalStateException("Failed to retrieve user");
-		}
-		var user = result.get();
-		return "Found user: " + user.getId();
+
+		Authentication authResponse = this.authenticationManager.authenticate(authRequest);
+		return authResponse.toString();
 	}
 
 	@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
 	@ExceptionHandler(DataAccessException.class)
-	public void handleRepositoryExceptions() {}
+	public ResponseEntity<String> handleRepositoryExceptions(Exception e) {
+		return new ResponseEntity<String>(e.toString(), HttpStatus.INTERNAL_SERVER_ERROR);
+	}
 }
